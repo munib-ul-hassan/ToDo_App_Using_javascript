78005d49ab819d67ded963d910df61a3d0596c2d
Munib <munibkhatri123@gmail.com>
2021-10-25 23:38:23 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+
+
+function enter() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("My_input").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("Mylist").appendChild(li);
+    }
+    document.getElementById("My_input").value = "";
+  
+    
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+}
+
+
+
+
